26cd6947312a90cf55ce5a190558d915cf268cce
ailenheft-rgb <ailen.heft@gmail.com>
2026-03-17 11:57:57 -0300

    App

---
+++ public/sw.js
@@ -0,0 +1,6 @@
+// Este trabajador (Service Worker) es el requisito mínimo 
+// para que los navegadores te ofrezcan el botón de "Instalar App".
+
+self.addEventListener('fetch', function(event) {
+  // Por ahora lo dejamos pasar todo normalmente
+});
